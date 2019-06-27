97ccbbe04673b0fdabcd9093c61d68ea48f206d6
Mussuranae <mandonnetdev@outlook.com>
2019-06-27 18:31:42 +0200

    changement contact form

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,31 +13,20 @@ class ContactController extends AbstractController
      */
     public function contactForm(Request $request, \Swift_Mailer $mailer)
     {
-        $url = $request->request->get('url');
+        $nom = $request->request->get('nom');
+        $prenom = $request->request->get('prenom');
+        $from = $request->request->get('email');
+        $title = $request->request->get('titre');
+        $contactMessage = $request->request->get('message');
 
-        if ($request->getMethod() == 'POST') {
-            $nom = $request->request->get('nom');
-            $prenom = $request->request->get('prenom');
-            $from = $request->request->get('email');
-            $title = $request->request->get('titre');
-            $contactMessage = $request->request->get('message');
+        $message = (new \Swift_Message($title))
+            ->setFrom('effesceau.mairie@outlook.fr')
+            ->setTo('effesceau.mairie@outlook.fr')
+            ->setReplyTo($from)
+            ->setBody("Mail de $nom  $prenom ($from).
+             Message :
+             $contactMessage");
 
-            $message = (new \Swift_Message($title))
-                ->setFrom('effesceau.mairie@outlook.fr')
-                ->setTo('effesceau.mairie@outlook.fr')
-                ->setReplyTo($from)
-                ->setBody("Mail de $nom  $prenom ($from).
-                 Message :
-                 $contactMessage");
-
-            $mailer->send($message);
-            //   $this->addFlash('success', "Votre message a bien été envoyé.");
-
-            return $this->redirect($url);
-        }
-
-        // $this->addFlash('danger', "Votre message n'a pas pu être envoyé, veuillez réessayer plus tard.");
-        return $this->redirectToRoute($url);
+        $mailer->send($message);
     }
-
 }
